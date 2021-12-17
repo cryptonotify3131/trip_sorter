8d1d0dc1229e3f2666ee7e31a5db385a50f73e37
cryptonotify3131 <96278433+cryptonotify3131@users.noreply.github.com>
2021-12-17 14:50:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,26 @@
 <?php
 
-$path = (new Trip_sorter())->index("Madrid", "New York");
+$path = (new TripSorter())->index("Madrid", "New York");
 
 if($path) {
     foreach($path as $row) {
-        echo "From " . $row['from'] . " to " . $row['to'] . " with transport " . $row['transport_type'] . ($row['transport_number'] ? (" (No. " . $row['transport_number'] . ")") : "") . ($row['seat'] ? (", seat " . $row['seat']) : "") . "<br>";
+        echo "From " . $row['from'] . " to " . $row['to'] . " with transport " . $row['transport_type'] . 
+        ($row['transport_number'] ? (" (No. " . $row['transport_number'] . ")") : "") . 
+        ($row['seat'] ? (", seat " . $row['seat']) : "") . "<br>";
     }
 } else {
     echo "No path found";
 }
 
 /**
- * Trip_sorter_class is a class that process to calculate the path from starting location to destination
+ * TripSorter_class is a class that process to calculate the path from starting location to destination
  * 
- * @package Trip_sorter
+ * @package TripSorter
  */
-class Trip_sorter {
+class TripSorter 
+{
 
-    private $boarding_card_set = [
+    private $boardingCardSet = [
         [
             'from' => 'Madrid',
             'to' => 'Barcelona',
@@ -55,7 +58,7 @@ class Trip_sorter {
      */
     public function index($from, $to) 
     {
-        return $this->calculate_path($from, $to);
+        return $this->calculatePath($from, $to);
     }
 
     /** 
@@ -65,41 +68,39 @@ class Trip_sorter {
      * @param string $to destination
      * @param array $path to store current path
      * @return array the path from starting point to destination, return empty array [] if path not found
-     * @access private docstore.mik.ua/orelly/webprog/pcook/ch21_09.htm
+     * @access private
      */
-    private function calculate_path($from, $to, $path = []) 
+    private function calculatePath($from, $to, $path = []) 
     {
-        
-        $last_path = end($path);
-        $last_to = isset($last_path['to']) ? $last_path['to'] : $from;
+        $lastPath = end($path);
+        $lastTo = isset($lastPath['to']) ? $lastPath['to'] : $from;
 
         if($path) {
-            $destination_list = array_merge(array_column($path, 'from'), array_column($path, 'to'));
+            $destinationList = array_merge(array_column($path, 'from'), array_column($path, 'to'));
         } else {
-            $destination_list = [];
+            $destinationList = [];
         }
 
-        $next_path = [];
+        $nextPath = [];
         
-        foreach ($this->boarding_card_set as $row) {
+        foreach ($this->boardingCardSet as $row) {
             // To prevent back to previous destination
-            if (!in_array($to, $destination_list)) {
-                if ($row['from'] == $last_to) {
-                    $next_path = $row;
+            if (!in_array($to, $destinationList)) {
+                if ($row['from'] == $lastTo) {
+                    $nextPath = $row;
                 }
             }
 
-            if ($row['from'] == $last_to && $row['to'] == $to) {
+            if ($row['from'] == $lastTo && $row['to'] == $to) {
                 $path[] = $row;
                 return $path;
             }
         }
 
-        if($next_path) {
-            $path[] = $next_path;
-            return $this->calculate_path($last_to, $to, $path);
+        if($nextPath) {
+            $path[] = $nextPath;
+            return $this->calculatePath($lastTo, $to, $path);
         }
-
 
         return [];
     }
